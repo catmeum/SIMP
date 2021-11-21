5bc1dbaf940d993b1a4fc9c94c6793de55f77ecf
gap6768 <gap6768@rit.edu>
2021-11-21 18:49:55 -0500

    Attainable values via exiftags and array access

--- static/ts/index.ts
+++ static/ts/index.ts
@@ -1,18 +1,26 @@
 import {ExifParserFactory, ExifTags} from "ts-exif-parser";
+import {type} from "os";
 
 async function handleFileSelect(evt: any) {
     let files: File[] = evt.target.files; // FileList object
-    const filePairs: {file: File, tags: ExifTags}[] = [];
+    //const filePairs: {file: File, tags: ExifTags}[] = [];
+    const testArr = [];
     for (let file of files) {
         let buffer = await file.arrayBuffer();
         let parser = ExifParserFactory.create(buffer);
         let output = parser.parse();
         const tags = output.tags;
-        filePairs.push({file, tags})
+        tags.OriginalRawFileName = file.name;
+        testArr.push(tags);
+        //filePairs.push({file, tags});
     }
-    const filteredFilePairs = filePairs.filter(({file, tags}) => tags.LensMake === "Apple");
-    console.log({filePairs, filteredFilePairs});
-    let fileName = "TEST STRING"
+    console.log(testArr[0].OriginalRawFileName);
+    //const filteredFilePairs = filePairs.filter(({file, tags}) => tags.Make === "Google");
+    //console.log({filePairs, filteredFilePairs});
+   /*for (let k in filePairs) {
+       console.log();
+   }*/
+    let fileName = "TEST STRING";
     document.getElementById("data").innerHTML = fileName;
     // @ts-ignore
     //window.map = map;
